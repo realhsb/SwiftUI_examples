b5524335a4ea4ee0710b60ce8a86c8a5cf5c58fa
realhsb <realhsb@gmail.com>
2024-04-19 02:27:15 +0900

    CarouselCardList 파일 생성

---
+++ CarouselCardList/CarouselCardList/CarouselCardListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselCardListApp.swift
+//  CarouselCardList
+//
+//  Created by Subeen on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CarouselCardListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
