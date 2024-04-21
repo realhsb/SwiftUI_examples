5ebc2bddfa50fbe1f990327e3c223fe35e6a80c1
realhsb <realhsb@gmail.com>
2024-04-21 14:35:20 +0900

    InteractiveBarCharts 파일 생성

---
+++ InteractiveBarCharts/InteractiveBarCharts/InteractiveBarChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveBarChartsApp.swift
+//  InteractiveBarCharts
+//
+//  Created by Subeen on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveBarChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
