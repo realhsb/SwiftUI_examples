c832d30b3bf9d6044d54764e553db39cbd6873d5
realhsb <realhsb@gmail.com>
2024-04-21 16:27:16 +0900

    Bar Chart + Rule Mark

--- InteractiveBarCharts/InteractiveBarCharts/ContentView.swift
+++ InteractiveBarCharts/InteractiveBarCharts/ContentView.swift
@@ -6,18 +6,76 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    
+    @State var selectedDay: Date?
+    @State var selectedView: Int?
+    
+    var previews: [DailyViews] = [
+        .init(dateStr: "1월 1일", views: 32),
+        .init(dateStr: "1월 2일", views: 2),
+        .init(dateStr: "1월 3일", views: 300),
+        .init(dateStr: "1월 4일", views: 999),
+        .init(dateStr: "1월 5일", views: 12),
+        .init(dateStr: "1월 6일", views: 1),
+        .init(dateStr: "1월 7일", views: 99),
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Chart{
+                ForEach(previews) { day in
+                    BarMark(
+                        x: .value("Day", day.date, unit: .day),
+                        y: .value("Views", day.views)
+                        
+                    )
+                    .foregroundStyle(.orange)
+                }
+                
+                if let selectedView {
+                    RuleMark(
+                        
+                        y: .value("Views", selectedView)
+                    )
+                    .annotation(
+                        position: .top,
+                        alignment: .leading,
+                        overflowResolution: .init(
+                            x: .disabled,
+                            y: .fit(to: .chart)
+                        )
+                    ) {
+                        Text("\(selectedView )")
+                    }
+                }
+            }
+            .frame(height: 200)
+            .padding(.horizontal, 20)
+            .chartXSelection(value: $selectedDay)
+            .chartYSelection(value: $selectedView)
         }
-        .padding()
     }
 }
+
+
+struct DailyViews: Identifiable {
+    let id = UUID()
+    let dateStr: String // DateFormatter로 변환
+    let views: Int
+    
+    var date: Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM월 dd일"
+        
+        let convertedDate = dateFormatter.date(from: dateStr)!
+        
+        return convertedDate
+    }
+}
+
 
 #Preview {
     ContentView()
---
+++ InteractiveBarCharts/InteractiveBarCharts/DailyViews.swift
@@ -0,0 +1,30 @@
+//
+//  DailyViews.swift
+//  InteractiveBarCharts
+//
+//  Created by Subeen on 4/21/24.
+//
+
+import Foundation
+
+//struct DailyViews: Identifiable {
+//    let id = UUID()
+//    let dateStr: Date // DateFormatter로 변환
+//    let views: Int
+
+//    var date: String {
+//        let firstDateFormatter = DateFormatter()
+//        firstDateFormatter.dateFormat = "yyyy/MM/dd HH:mm"
+//        
+//        let firstDate = firstDateFormatter.date(from: dateStr)!
+//        
+//        let secondDateFormatter = DateFormatter()
+//        
+//        secondDateFormatter.dateFormat = "MM월 dd일"
+//        
+//        let secondDate = secondDateFormatter.string(from: firstDate)
+//
+//        
+//        return secondDate
+//    }
+//}
